1f797a77b1297560b51fb801293318420c56ece0
doxus <doxuswastaken+github@protonmail.com>
2024-05-06 18:21:05 +0200

    Fix issues with company backend

--- sqf-mode-company-backend.el
+++ sqf-mode-company-backend.el
@@ -1,18 +1,18 @@
 (require 'company)
 (require 'sqf-mode)
 
-(defvar company-sqf-completions (append sqf-mode-keywords '("and" "do" "else" "exit" "exitWith" "false" "for" "forEach" "forEachMember" "forEachMemberAgent" "forEachMemberTeam" "from" "if" "in" "nil" "not" "or" "then" "to" "true" "try" "waitUntil" "with" "while")))
+(defconst company-sqf-completions (append sqf-mode-keywords '("and" "do" "else" "exit" "exitWith" "false" "for" "forEach" "forEachMember" "forEachMemberAgent" "forEachMemberTeam" "from" "if" "in" "nil" "not" "or" "then" "to" "true" "try" "waitUntil" "with" "while")))
 
 (defun company-sqf-backend (command &optional arg &rest ignored)
   (interactive (list 'interactive))
 
   (cl-case command
     (interactive (company-begin-backend 'company-sqf-backend))
-    (prefix (and (eq major-mode 'fundamental-mode)
+    (prefix (and (eq major-mode 'sqf-mode)
                 (company-grab-symbol)))
     (candidates
-    (remove-if-not
+    (cl-remove-if-not
       (lambda (c) (string-prefix-p arg c))
-      sqf-company-completions))))
+      company-sqf-completions))))
 
 (add-to-list 'company-backends 'company-sqf-backend)
